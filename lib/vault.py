4972d1882ec1b602e628f5eff94339458f89a9e8
Leopoldo Caballero <git@pitakill.net>
2020-02-26 14:50:53 -0600

    Cleanup the JSON responses
    1. Why is this change necessary?
    We need to strip the null and empty strings from the json
    
    2. How does it address the issue?
    This uses a function that cleanups
    
    3. What side effects does this change have?
    None

--- lib/vault.py
+++ lib/vault.py
@@ -24,78 +24,68 @@ class Vault:
 
     def get_identity(self):
         return self.client.lookup_token()['data']
-    
+
     def get_audit_devices(self):
         return self.client.sys.list_enabled_audit_devices()['data']
-    
+
     def get_auth_methods(self):
         return self.client.sys.list_auth_methods()['data']
 
     def is_initialized(self):
         return self.client.is_initialized()
-    
+
     def get_policies(self):
         return self.client.sys.list_policies()['data']['policies']
-    
+
     def get_list_namespaces(self):
         return self.client.sys.list_namespaces()
-    
+
     def get_secrets_engine_list(self):
         return self.client.sys.list_mounted_secrets_engines()['data']
 
     def uptime(self):
         return "We need to improve this"
 
-    def get_metricts(self):
-        return requests.get(
-            self.addr + "/v1/sys/metrics?format=", 
+    def cleanup_json(self, data):
+        cleaned = dict()
+        for k,v in data.items():
+            if v is None or v == '':
+                print(k,v)
+                continue
+
+            cleaned[k] = v
+        return cleaned
+
+    def query_vault(self, path):
+        url = self.addr + path
+        return self.cleanup_json(requests.get(
+            url,
             headers={'X-Vault-Token': self.token}
-        ).json()
+        ).json())
+
+    def get_metricts(self):
+        return self.query_vault("/v1/sys/metrics?format=")
 
     def get_expire_leases(self):
-        return requests.get(
-            self.addr + "/v1/sys/leases", 
-            headers={'X-Vault-Token': self.token}
-        ).json()
+        return self.query_vault("/v1/sys/leases")
 
     def get_configuration(self):
-        return requests.get(
-            self.addr + "/v1/sys/config/state/sanitized", 
-            headers={'X-Vault-Token': self.token}
-        ).json()
+        return self.query_vault("/v1/sys/config/state/sanitized")
 
     def get_health(self):
-        return requests.get(
-            self.addr + "/v1/sys/health", 
-            headers={'X-Vault-Token': self.token}
-        ).json()
+        return self.query_vault("/v1/sys/health")
 
     def get_general_information(self):
-        return requests.get(
-            self.addr + "/v1/sys/host-info", 
-            headers={'X-Vault-Token': self.token}
-        ).json()
+        return self.query_vault("/v1/sys/host-info")
 
     def get_version(self):
-        return requests.get(
-            self.addr + "/v1/sys/health", 
-            headers={'X-Vault-Token': self.token}
-        ).json()['version']
+        return self.query_vault("/v1/sys/health")
 
     def get_features(self):
-        return requests.get(
-            self.addr + "/v1/sys/license", 
-            headers={'X-Vault-Token': self.token}
-        ).json()
+        return self.query_vault("/v1/sys/license")
 
     def get_integrated_apps(self):
-        return requests.get(
-            self.addr + "/v1/sys/mounts", 
-            headers={'X-Vault-Token': self.token}
-        ).json()
+        return self.query_vault("/v1/sys/mounts")
 
     def wrapping(self, function):
-        return requests.get(
-            self.addr + "/v1/sys/wrapping/" + function, 
-            headers={'X-Vault-Token': self.token}
-        ).json()
+        return self.query_vault("/v1/sys/wrapping/" + function)
